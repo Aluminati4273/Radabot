ca01a714637dedfc095bc20dfd5fe420897105f5
Aluminati4273 <ljenks@hotsprings1.org>
2017-12-09 08:35:58 -0700

    Servo claw value corrections Slideplate speed corrections

--- TeamCode/src/main/java/DriveByEncoder.java
+++ TeamCode/src/main/java/DriveByEncoder.java
@@ -33,7 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+//
 
 
 @Autonomous(name="DriveByEncoder", group="Auto")
