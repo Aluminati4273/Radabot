2e88dde30c58d18f691f171574267a068bd6ca9e
TheCoach <ljenks@hotsprings1.org>
2018-01-27 15:31:46 -0700

    Fixed AutoBlue1 and AutoBlue 2 - need tested

--- TeamCode/src/main/java/AutoBlue1.java
+++ TeamCode/src/main/java/AutoBlue1.java
@@ -1,9 +1,9 @@
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import android.graphics.Color;
 
 /**
  * Created by londonjenks on 12/5/17.
@@ -19,6 +19,16 @@ public class AutoBlue1 extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
+    //===================== Color Sensor Setup ===========================
+
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    float hsvValues[] = {0F,0F,0F};
+
+    // values is a reference to the hsvValues array.
+    final float values[] = hsvValues;
+    //====================================================================
+
+
     //
     private int autoTrip = 0;
     private boolean direction = false;
@@ -27,56 +37,78 @@ public class AutoBlue1 extends LinearOpMode {
 
         robot.init(hardwareMap);
 
-        robot.blueColor.setI2cAddress(I2cAddr.create7bit(0x20));
+        //set color sensor address
+        robot.blueColor.setI2cAddress(I2cAddr.create7bit(0x10));
 
+        //wait for player to hit start
         waitForStart();
 
+        // convert the RGB values to HSV values.
+        Color.RGBToHSV(robot.blueColor.red() * 8, robot.blueColor.green() * 8, robot.blueColor.blue() * 8, hsvValues);
 
-        robot.jewelServoBlue.setPosition(0.95);
+        //move the servo into detect position
+        robot.jewelServoBlue.setPosition(1.0);
 
         robot.bluePlateDistance(0.5, 500);
 
         while(opModeIsActive() && autoTrip <1)
         {
-            if (robot.blueColorNumber < 4 && robot.blueColorNumber > 2)
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Clear", robot.blueColor.alpha());
+            telemetry.addData("Red ", robot.blueColor.red());
+            telemetry.addData("Green", robot.blueColor.green());
+            telemetry.addData("Blue ", robot.blueColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            telemetry.update();
+
+
+            if (robot.blueColor.red() > robot.blueColor.blue() && robot.blueColor.red() > robot.blueColor.green())
             {
+                //drive backward power and distance
                 robot.driveBackDistance(1.0, 700);
+
+                //increment so robot only passes through sequence once
                 autoTrip ++;
+
+                //helps track the direction the robot moved following color detection
                 direction = true;
             }
 
             //if color sensor is blue, drive backward
-            if(robot.blueColorNumber < 11 && robot.blueColorNumber > 8)
+            if (robot.blueColor.blue() > robot.blueColor.red() && robot.blueColor.blue() > robot.blueColor.green())
             {
 
-
+                //drive forward power and distance
                 robot.driveForwardDistance(1.0, 200);
+
+                // increment so robot only passes through sequence once
                 autoTrip++;
             }
         }
 
+        //retract slide plate to cener
         robot.bluePlateDistance(0.5,-500);
 
+        //wait a quarter of a second
         sleep(250);
 
+        //return color sensor arm to start position
         robot.jewelServoBlue.setPosition(robot.JEWEL_SERVO_BLUE_START);
 
+        //wait a quarter of a second
         sleep (250);
 
 
+        //if the robot drove forward to hit the red jewel then drive forward less to park
         if(direction = true) {
             robot.driveForwardDistance(1.0, 1200);
         }
+
+        // if the robot drove backward to hit the red jewel then drive forward more to park
         else{
             robot.driveForwardDistance(1.0, 1600);
         }
-
-
-
-
-
-
-
 
 
     }
--- TeamCode/src/main/java/AutoBlue2.java
+++ TeamCode/src/main/java/AutoBlue2.java
@@ -2,13 +2,15 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import android.graphics.Color;
 
 /**
  * Created by londonjenks on 12/5/17.
  */
 
 
-@Autonomous(name="AutoBlue1", group="Auto")
+@Autonomous(name="AutoBlue2", group="Auto")
 
 public class AutoBlue2 extends LinearOpMode {
 
@@ -17,71 +19,105 @@ public class AutoBlue2 extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    //used to restrict while loop for color sensing to one trip
-    private int autoTrip = 0;
+    //===================== Color Sensor Setup ===========================
 
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    float hsvValues[] = {0F,0F,0F};
+
+    // values is a reference to the hsvValues array.
+    final float values[] = hsvValues;
+    //====================================================================
+
+
+    //
+    private int autoTrip = 0;
+    private boolean direction = false;
     @Override
     public void runOpMode() {
 
         robot.init(hardwareMap);
 
-        //set the correct color sensor address
-        robot.blueColor.setI2cAddress(I2cAddr.create7bit(0x20));
+        //set color sensor address
+        robot.blueColor.setI2cAddress(I2cAddr.create7bit(0x10));
 
+        //wait for player to hit start
         waitForStart();
 
-        //move the color sensor arm into position
-        robot.jewelServoBlue.setPosition(0.95);
+        // convert the RGB values to HSV values.
+        Color.RGBToHSV(robot.blueColor.red() * 8, robot.blueColor.green() * 8, robot.blueColor.blue() * 8, hsvValues);
 
-        //slide plate toward the jewel
+        //move the servo into detect position
+        robot.jewelServoBlue.setPosition(1.0);
+
         robot.bluePlateDistance(0.5, 500);
 
         while(opModeIsActive() && autoTrip <1)
         {
-            //if colors sensor is blue, drive backward
-            if (robot.blueColorNumber < 4 && robot.blueColorNumber > 2)
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Clear", robot.blueColor.alpha());
+            telemetry.addData("Red ", robot.blueColor.red());
+            telemetry.addData("Green", robot.blueColor.green());
+            telemetry.addData("Blue ", robot.blueColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            telemetry.update();
+
+
+            if (robot.blueColor.red() > robot.blueColor.blue() && robot.blueColor.red() > robot.blueColor.green())
             {
-                //drive backward
-                robot.driveBackDistance(1.0, 500);
-                //increment autoTrip
+                //drive backward power and distance
+                robot.driveBackDistance(1.0, 700);
+
+                //increment so robot only passes through sequence once
                 autoTrip ++;
+
+                //helps track the direction the robot moved following color detection
+                direction = true;
             }
 
-            //if color sensor is red, drive forward
-            if(robot.blueColorNumber < 11 && robot.blueColorNumber > 8)
+            //if color sensor is blue, drive backward
+            if (robot.blueColor.blue() > robot.blueColor.red() && robot.blueColor.blue() > robot.blueColor.green())
             {
-                //drive forward
-                robot.driveForwardDistance(1.0, 500);
-                //increment autoTrip
+
+                //drive forward power and distance
+                robot.driveForwardDistance(1.0, 200);
+
+                // increment so robot only passes through sequence once
                 autoTrip++;
             }
         }
 
-        //retract slide plate into starting position
+        //retract slide plate to center
         robot.bluePlateDistance(0.5,-500);
 
-        // wait a quarter of a second
+        //wait a quarter of a second
         sleep(250);
 
-        //retract color sensor arm into stating position
+        //return color sensor arm to start position
         robot.jewelServoBlue.setPosition(robot.JEWEL_SERVO_BLUE_START);
 
-        // wait a quarter of a second
+        //wait a quarter of a second
         sleep (250);
 
-        //drive forward toward the parking zone
-        robot.driveForwardDistance(0.7, 900);
 
-        // wait a quarter of a second
+        //if the robot drove forward to hit the red jewel then drive forward less to park
+        if(direction = true) {
+            robot.driveForwardDistance(1.0, 600);
+        }
+
+        // if the robot drove backward to hit the red jewel then drive forward more to park
+        else{
+            robot.driveForwardDistance(1.0, 900);
+        }
+
+        //wait a quarter of a second
         sleep (250);
 
-        //turn toward the parking zone
-        robot.turnRightDistance(0.7, 500);
+        //turn to the right to face the park area
+        robot.turnRightDistance(0.7, 400);
 
-        // wait a quarter of a second
-        sleep (250);
 
-        //drive into the parking zone
+        //drive forward to the park area
         robot.driveForwardDistance(0.7, 700);
     }
 
